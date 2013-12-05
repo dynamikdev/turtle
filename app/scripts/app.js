a65aa5ceb1b0d253dc18683de6ed957278695081
Philippe Tartaise <ptartaise@dynamikdev.com>
2013-12-05 14:01:00 +0100

    add readme

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,13 +1,33 @@
 'use strict';
+angular.module('turtleApp', ['ngRoute'])
+    .config(function ($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/main.html',
+                controller: 'MainCtrl'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    })
+    .factory('turtle', function () {
+        var Turtle;
+        Turtle = (function () {
+            function Turtle() {
+            }
 
-angular.module('turtleApp', [])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+            Turtle.prototype.x = 200;
+            Turtle.prototype.y = 200;
+            Turtle.prototype.angle = -90;
+            Turtle.prototype.move = function (distance) {
+                this.x = distance * Math.cos(this.angle);
+                this.y = distance * Math.sin(this.angle);
+                return this;
+            };
+            Turtle.prototype.turn = function (angle) {
+                this.angle = angle;
+                return this;
+            };
+        })();
+        return Turtle;
+    });
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,37 +1,35 @@
 'use strict';
 
-angular.module('turtleApp',[])
-    .factory('turtle',function(){
-        var turtle;
-        turtle = new Turtle();
-        return turtle;
-
-    })
-  .controller('MainCtrl', function ($scope,$log,$turtle) {
-
+//angular.module('turtleApp',['ngRoute'])
+//    .factory('turtle',function(){
+//        var turtle;
+//        turtle = new Turtle();
+//        return turtle;
+//
+//    });
+var MainCtrl = function ($scope, $log) {
     $scope.X = 400;
     $scope.Y = 400;
     $scope.Angle = 90 - 360;
 
-    $scope.lines=[
+    $scope.lines = [
     ];
-    $scope.history=[
+    $scope.history = [
     ];
 
-        $scope.addLine = function(line){
-
+    $scope.addLine = function (line) {
         console.log(line);
         $scope.lines.push(angular.copy(line));
-      };
-    $scope.move = function(deplacement){
-        $scope.Angle =$scope.Angle - parseInt(deplacement.angle);
-        var x2 = $scope.X+(parseInt(deplacement.distance) * Math.cos(parseInt($scope.Angle)*Math.PI/180));
-        var y2 = $scope.Y-(parseInt(deplacement.distance) * Math.sin(parseInt($scope.Angle)*Math.PI/180));
-        $scope.lines.push({'x1':$scope.X ,'y1':$scope.Y,'x2':x2,'y2':y2});
+    };
+    $scope.move = function (deplacement) {
+        $scope.Angle = $scope.Angle - parseInt(deplacement.angle, 10);
+        var x2 = $scope.X + (parseInt(deplacement.distance, 10) * Math.cos(parseInt($scope.Angle, 10) * Math.PI / 180));
+        var y2 = $scope.Y - (parseInt(deplacement.distance, 10) * Math.sin(parseInt($scope.Angle, 10) * Math.PI / 180));
+        $scope.lines.push({'x1': $scope.X, 'y1': $scope.Y, 'x2': x2, 'y2': y2});
         $scope.X = x2;
-        $scope.Y=y2;
+        $scope.Y = y2;
 
         $log.log($scope);
-      };
+    };
 
-  });
+};
